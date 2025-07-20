7e6b6bcf519ee887438a6271e3677d04b234d7a7
matthew harwood <matthhar12@gmail.com>
2025-07-20 08:39:08 -0700

    Initial project setup with Bevy and assets
    Add .gitignore, Cargo.toml, and configure Bevy dependencies. Include default grid tile asset and implement basic window and sprite setup in main.rs. Update IDE configuration for source and target folders.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use bevy::prelude::*;
+use bevy::window::WindowResolution;
+
+pub const WINDOW_WIDTH: f32 = 1280.0;
+pub const WINDOW_HEIGHT: f32 = 720.0;
+pub const GRID_WIDTH: usize = 65;
+pub const GRID_HEIGHT: usize = 31;
+pub const TILE_SIZE: f32 = 19.0;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            title: "Arenic".to_string(),
+            resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+            ..default()
+        }),
+        ..default()
+    })).add_systems(Startup, setup).run();
+}
+
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Transform::from_xyz(-WINDOW_WIDTH/2.0 + TILE_SIZE/2.0, WINDOW_HEIGHT/2.0 - TILE_SIZE/2.0, 0.0))
+        .with_children(|parent| { parent.spawn(
+            Sprite {
+                image: asset_server.load("default_grid_tile.png"),
+                custom_size: Some(Vec2::new(TILE_SIZE,TILE_SIZE)),
+                ..default()
+            }
+        );
+    });
+}
